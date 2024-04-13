352423be6f3a9bf0045d5d3bb005fdb952d102d3
yokamoto5742-h <yokamoto5742@gmail.com>
2024-04-13 15:42:55 +0900

    1st commit

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
+
 class Config(object):
     DEBUG = True
     SECRET_KEY = 'secret_key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///memodb.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://",
+                                                                     "postgresql://") or 'sqlite:///memodb.db'
     SQLALCHEMY_ECHO = False
