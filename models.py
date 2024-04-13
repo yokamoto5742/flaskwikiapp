ee6d34be297488382ffe91c647bdfeb1a18a3a8c
yokamoto5742-h <yokamoto5742@gmail.com>
2024-04-13 18:28:03 +0900

    1st commit

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 from flask_login import UserMixin
 from sqlalchemy.orm import relationship
+from passlib.hash import sha256_crypt
 
 db = SQLAlchemy()
 
@@ -20,13 +21,13 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
     memos = relationship('Memo', back_populates='user')
 
     def set_password(self, password):
-        hashed_password = generate_password_hash(password)
-        truncated_password = hashed_password[:100]
+        hashed_password = sha256_crypt.hash(password)
+        truncated_password = hashed_password[:120]
         self.password = truncated_password
 
     def check_password(self, password):
