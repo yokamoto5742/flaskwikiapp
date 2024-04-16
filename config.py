cedbffa20c574cce9a601d81426887f16f2f4bad
yokamoto5742-h <yokamoto5742@gmail.com>
2024-04-16 16:01:40 +0900

    1st commit

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from postgresql import SQLALCHEMY_DATABASE_URI as DATABASE_URI
 class Config(object):
     DEBUG = True
     SECRET_KEY = 'secret_key'
-    SQLALCHEMY_DATABASE_URI = DATABASE_URI
 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://",
-    #                                                                  "postgresql://") or 'sqlite:///memodb.db'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://",
+                                                                     "postgresql://")
+    # ローカル環境のPostgreSQL に接続する場合は、以下の行を有効にしてください
+    # SQLALCHEMY_DATABASE_URI = DATABASE_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_ECHO = True
